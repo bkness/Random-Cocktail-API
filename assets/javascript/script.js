8096faacb8693f42a2b9af43f32fb58df957342d
bkness <kbrandon863@gmail.com>
2023-09-30 22:20:56 -0700

    layout is looking good and functionality is working nicely.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     var generateButton = document.querySelector(".btn-primary");
+    var cardBody = document.querySelector(".card-body");
+    var cardFooter = document.querySelector(".card-footer");
+    var cocktailInfo = document.querySelectorAll('.cocktail-info');
+    var cocktailInstructions = document.getElementById('cocktailInstructions').querySelector('div');
+
+    cocktailInfo.forEach(function (element) {
+        element.style.display = 'none';
+    });
 
     generateButton.addEventListener('click', function () {
-        var cardBody = document.querySelector(".card-body");
-        var cardFooter = document.querySelector(".card-footer");
+        cocktailInstructions.style.display = 'none';
+        cocktailInfo.forEach(function (element) {
+            element.style.display = 'block';
+        });
 
-        // function to fetch my data 
         var getRandomCocktail = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
         fetch(getRandomCocktail)
@@ -16,25 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
                 return response.json();
             })
             .then(function (data) {
-                var cocktailName = document.getElementById('cocktailName');
-                var cocktailImage = document.getElementById('cocktailImage');
-                var cocktailCategory = document.getElementById('cocktailCategory');
-                var cocktailAlcoholic = document.getElementById('cocktailAlcoholic');
-                var cocktailGlass = document.getElementById('cocktailGlass');
-                var ingredientList = document.getElementById('ingredientList');
-                var cocktailInstructions = document.getElementById('cocktailInstructions');
+                var cocktailName = document.getElementById('cocktailName').querySelector('div');
+                var cocktailImage = document.getElementById('cocktailImage').querySelector('img');
+                var cocktailCategory = document.getElementById('cocktailCategory').querySelector('div');
+                var cocktailAlcoholic = document.getElementById('cocktailAlcoholic').querySelector('div');
+                var cocktailGlass = document.getElementById('cocktailGlass').querySelector('div');
+                var ingredientList = document.getElementById('ingredientList').querySelector('ul');
+                var cocktailInstructions = document.getElementById('cocktailInstructions').querySelector('div');
 
-                // Set values
                 cocktailName.innerText = data.drinks[0].strDrink;
                 cocktailImage.src = data.drinks[0].strDrinkThumb;
                 cocktailCategory.innerText = 'Category: ' + data.drinks[0].strCategory;
                 cocktailAlcoholic.innerText = 'Alcoholic: ' + data.drinks[0].strAlcoholic;
                 cocktailGlass.innerText = 'Glass: ' + data.drinks[0].strGlass;
 
-                // Clear ingredient list
                 ingredientList.innerHTML = '';
-
-                // Populate ingredient list
                 for (var i = 1; i <= 15; i++) {
                     var ingredient = data.drinks[0]['strIngredient' + i];
                     var measure = data.drinks[0]['strMeasure' + i];
@@ -48,7 +53,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 cocktailInstructions.innerText = data.drinks[0].strInstructions;
 
-                // Show card
                 cardBody.style.display = 'block';
                 cardFooter.style.display = 'block';
             })
