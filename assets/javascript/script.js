a512356a44609679678b88764939f0e4df4044cb
bkness <kbrandon863@gmail.com>
2023-09-30 18:31:52 -0700

    set up some bootstrap layouts and set up some css styles

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,6 @@ var getRandomCocktail = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 fetch(getRandomCocktail) 
     .then(function (response) {
         return response.json();
-        console.log(response)
     })
     .then(function (data){
         console.log(data)
